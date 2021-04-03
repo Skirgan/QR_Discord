3c7f6d02caeda26354117a640d63b7bbf2074f04
Skirgan <71898047+Skirgan@users.noreply.github.com>
2021-04-03 11:15:57 +0300

    Add files via upload

---
+++ QRCode/main.py
@@ -0,0 +1,21 @@
+import discord
+import qrcode
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!') # Вместо ! может быть любые символы
+
+@bot.event
+async def on_ready():
+    print('[QR] Ready')
+
+@bot.command()
+async def шифр(ctx, *, text):
+    qrim = qrcode.make(text)
+    qrim.save('qrcode.png')
+    with open('qrcode.png', 'rb') as f:
+        picture = discord.File(f)
+    embed = discord.Embed(title='',colour=discord.Colour.light_gray())
+    embed.add_field(name=f'Шифровка в QR',value=f'**Зашифровано**: `{text}`')
+    embed.set_image(url="attachment://qrcode.png")
+    await ctx.send(file=discord.File('qrcode.png'),embed=embed)
+
+bot.run('Токен') # Не забудьте вставить свой токен!!!
